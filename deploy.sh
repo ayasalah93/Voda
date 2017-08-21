06f472b0cb84044f33d8a1c3e2025a2f384cfc26
Aya Salah <aya.salah_93@outlook.com>
2017-08-21 15:43:04 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 if [[ $(kubectl get deployments | grep testkube) ]]; then
 echo "deployments exists"
 else
-kubectl run testkube --image=ayasalah93/voda --replicas=2 --output=yaml --dry-run > "ayasalah93/voda-rc.yaml"
-kubectl create -f "ayasalah93/voda-rc.yaml"
+kubectl run testkube --image=ayasalah93/voda --replicas=2 --output=yaml --dry-run > "voda-rc.yaml"
+kubectl create -f "voda-rc.yaml"
 fi
